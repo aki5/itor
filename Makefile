9631fa7e2fee2b8f260698b4fb4160d0165d4660
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-24 21:25:27 -0700

    basic undo

--- Makefile
+++ Makefile
@@ -4,7 +4,10 @@ all: itor
 
 include $(ROOT)/libdraw3/libdraw3.mk
 
-CFLAGS=-O3 -fomit-frame-pointer -I$(ROOT)/libdraw3 #-static-libasan -fsanitize=address
+CFLAGS=\
+	-O3 -fomit-frame-pointer \
+	-I$(ROOT)/libdraw3 \
+	-static-libasan -fsanitize=address \
 
 itor: main.o $(LIBDRAW3)
 	$(CC) $(CFLAGS) -o $@ main.o $(LIBDRAW3) $(LIBDRAW3_LIBS)
--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-
+/*
+ *	Itor is a "what you get is what you get" editor
+ */
 #include "os.h"
 #include "draw3.h"
 
@@ -16,6 +18,9 @@ int uoff, voff;
 Line *line0;
 Line *lineend;
 
+char *clip;
+int nclip, aclip;
+
 char *text;
 int ntext, atext;
 
@@ -29,14 +34,21 @@ int nmark;
 void
 drawtext(Rect dstr, short *sel0, short *sel1)
 {
-	int sel;
+	int insel, dx;
 	Rect r;
 
-	sel = 0;
+	insel = 0;
 
 	Rect lr;
 	int code, off;
 	char *sp;
+
+	/* add a phony newline, helps with selection */
+	if(ntext == atext){
+		atext++;
+		text = realloc(text, atext);
+	}
+	//text[ntext++] = '\n';
 
 	r.u0 = dstr.u0 + uoff;
 	r.v0 = dstr.v0 + voff;
@@ -55,28 +67,27 @@ drawtext(Rect dstr, short *sel0, short *sel1)
 			lr.uend = 32767;
 			break;
 		case '\t':
-			lr = rect(r.u0, r.v0, r.u0+3*fontem(), r.v0+linespace());
+			dx = 3*fontem();
+			lr = rect(r.u0, r.v0, r.u0+dx, r.v0+linespace());
 			break;
 		default:
 			lr = drawchar(&screen, r, code, fgcolor);
 			break;
 		}
-		if(nmark < 2 && ptinrect(sel0, &lr)){
-			sel ^= 1;
+		if(nmark < 2 && ptinrect(sel0, &lr))
 			mark[nmark++] = sp-text;
-		}
-		if(nmark < 2 && ptinrect(sel1, &lr)){
-			sel ^= 1;
+		if(nmark < 2 && ptinrect(sel1, &lr))
 			mark[nmark++] = sp-text;
+
+		if(nmark > 0 && mark[0] == sp-text){
+			drawrect(&screen, rect(lr.u0-1, lr.v0, lr.u0+1, lr.vend), color(255,255,255,255));
+			insel = 1;
 		}
-		if(nmark == 2){
-			if(mark[0] == sp-text)
-				sel = 1;
-			if(mark[1] == sp-text)
-				sel = 0;
-		}
-		if(sel)
-			drawblend_back(&screen, lr, selcolor);
+		if(nmark > 1 && mark[1] == sp-text)
+			insel = 0;
+
+		if(insel)
+			blend_add_under(&screen, lr, selcolor);
 
 		if(code == '\n'){
 			r.u0 = dstr.u0 + uoff;
@@ -87,7 +98,77 @@ drawtext(Rect dstr, short *sel0, short *sel1)
 			r.u0 += rectw(&lr);
 		}
 	}
+	if(nmark < 2){
+		mark[nmark++] = sp-text;
+	}
+	if(nmark < 2){
+		mark[nmark++] = sp-text;
+	}
+	if(mark[0] == sp-text)
+		drawrect(&screen, rect(r.u0-1, r.v0, r.u0+1, r.vend), color(255,255,255,255));
+}
 
+enum {
+	OpInsert = 1,
+	OpErase = 2,
+};
+
+struct {
+	int op;
+	int mark;
+	char *str;
+	int len;
+} *ops;
+int nops, aops;
+
+void
+addop(int op, int mark, char *str, int len)
+{
+	if(nops >= aops){
+		aops = nops + 128;
+		ops = realloc(ops, aops * sizeof ops[0]);
+	}
+	ops[nops].op = op;
+	ops[nops].mark = mark;
+	ops[nops].str = malloc(len);
+	memcpy(ops[nops].str, str, len);
+	ops[nops].len = len;
+	nops++;
+}
+
+void
+insert(int mark0, char *str, int len, int is_undo)
+{
+	if(!is_undo)
+		addop(OpInsert, mark0, str, len);
+	if(ntext+len >= atext){
+		atext = ntext+len;
+		text = realloc(text, atext);
+	}
+	memmove(text+mark0+len, text+mark0, ntext-mark0);
+	ntext += len;
+	memcpy(text+mark0, str, len);
+}
+
+void
+erase(int mark0, int mark1, int is_undo)
+{
+	if(!is_undo)
+		addop(OpErase, mark0, text+mark0, mark1-mark0);
+	memmove(text+mark0, text+mark1, ntext-mark1);
+	ntext -= mark1-mark0;
+}
+
+void
+undo(void)
+{
+	if(nops > 0){
+		if(ops[nops-1].op == OpErase)
+			insert(ops[nops-1].mark, ops[nops-1].str, ops[nops-1].len, 1);
+		if(ops[nops-1].op == OpInsert)
+			erase(ops[nops-1].mark, ops[nops-1].mark+ops[nops-1].len, 1);
+		nops--;
+	}
 }
 
 int
@@ -99,13 +180,17 @@ main(int argc, char *argv[])
 	initdrawstr("/usr/share/fonts/truetype/droid/DroidSans.ttf"); //-Bold
 	//initdrawstr("/usr/share/fonts/truetype/freefont/FreeMono.ttf"); 
 	//initdrawstr("/usr/share/fonts/truetype/freefont/FreeSans.ttf"); 
-	setfontsize(9);
+	setfontsize(12);
 
-	fgcolor = allocimage(rect(0,0,1,1), color(150, 200, 80, 255));
+//	fgcolor = allocimage(rect(0,0,1,1), color(0, 0, 0, 255));
+//	fgcolor = allocimage(rect(0,0,1,1), color(170, 170, 170, 255));
+//	fgcolor = allocimage(rect(0,0,1,1), color(255, 255, 255, 255));
+
 //	bgcolor = allocimage(rect(0,0,1,1), color(255,255,255,255));
-	bgcolor = allocimage(rect(0,0,1,1), color(40,50,40,255));
-
+//	bgcolor = allocimage(rect(0,0,1,1), color(40,40,40,255));
+	fgcolor = allocimage(rect(0,0,1,1), color(150, 200, 80, 255));
 	selcolor = allocimage(rect(0,0,1,1), color(90,100,80,255));
+	bgcolor = allocimage(rect(0,0,1,1), color(0,40,40,255));
 
 	if(argc > 0){
 		int fd;
@@ -139,12 +224,22 @@ main(int argc, char *argv[])
 	for(;;){
 		repaint = 0;
 		for(inp = drawevents(&einp); inp < einp; inp++){
-			if(mark[0] > mark[1])
+			if(nmark == 2 && mark[0] > mark[1])
 				abort();
+			if(keystr(inp, "z") && (inp->on & KeyControl) != 0){
+				undo();
+			}
 			if(keystr(inp, "x") && (inp->on & KeyControl) != 0){
 				if(mark[0] < mark[1]){
-					memmove(text+mark[0], text+mark[1], ntext-mark[1]);
-					ntext -= mark[1]-mark[0];
+					if(aclip < mark[1]-mark[0]){
+						aclip = mark[1]-mark[0];
+						clip = realloc(clip, aclip);
+					}
+					memcpy(clip, text+mark[0], mark[1]-mark[0]);
+					nclip = mark[1]-mark[0];
+
+					erase(mark[0], mark[1], 0);
+
 					mark[1] = mark[0];
 					sel0[0] = 0;
 					sel0[1] = 0;
@@ -152,46 +247,42 @@ main(int argc, char *argv[])
 					sel1[1] = 0;
 				}
 			}
-			if(keystr(inp, "v") && (inp->on & KeyControl) != 0)
-				printf("PASTE!\n");
+			if(keystr(inp, "v") && (inp->on & KeyControl) != 0){
+				insert(mark[0], clip, nclip, 0);
+			}
 			if((keystr(inp, "q") || keystr(inp, "c")) && (inp->on & KeyControl) != 0)
 				exit(0);
 
 			if(mark[0] == mark[1]){
 				if((inp->on & KeyControl) == 0){
 					if(inp->begin & KeyStr){
-						int off;
-						off = strlen(inp->str);
-						if(ntext+off >= atext){
-							atext += atext;
-							text = realloc(text, atext);
-						}
-						memmove(text+mark[0]+off, text+mark[0], ntext-mark[0]);
-						ntext += off;
-						memcpy(text+mark[0], inp->str, off);
-						mark[0] = mark[1] = mark[0]+off;
+						int len;
+						len = strlen(inp->str);
+						insert(mark[0], inp->str, len, 0);
+						mark[0] = mark[1] = mark[0]+len;
 					}
 					if(inp->begin & KeyBackSpace){
 						if(mark[0] > 0){
-							memmove(text+mark[0]-1, text+mark[0], ntext-mark[0]);
-							ntext--;
-							mark[0] = mark[1] = mark[0]-1;
+							if(ntext > 0){
+								erase(mark[0]-1, mark[0], 0);
+								mark[0] = mark[1] = mark[0]-1;
+							}
 						}
 					}
 				}
-			} else {
-				char *p, *q, *ep;
-				if(keystr(inp, "\t")){
-					int code, off;
-					ep = text+mark[1];
-					for(q = p = text+mark[0]; p < ep; p += off){
-						code = utf8decode(p, &off, ep-p);
-					} 
+				if(keypress(inp, KeyLeft)){
+					if(mark[0] > 0)
+						mark[0]--;
+					mark[1] = mark[0];
 				}
-			}
+				if(keypress(inp, KeyRight)){
+					if(mark[0] < ntext)
+						mark[0]++;
+					mark[1] = mark[0];
+				}
+			} 
 
 			if(select == 0 && mousebegin(inp) == Mouse1){
-				//printf("select begin %d,%d!\n", inp->xy[0], inp->xy[1]);
 				sel0[0] = inp->xy[0]-uoff;
 				sel0[1] = inp->xy[1]-voff;
 				sel1[0] = inp->xy[0]-uoff;
@@ -207,10 +298,10 @@ main(int argc, char *argv[])
 			if(select && mouseend(inp) == Mouse1){
 				sel1[0] = inp->xy[0]-uoff;
 				sel1[1] = inp->xy[1]-voff;
-				//printf("select end: %d,%d,%d,%d!\n", sel0[0], sel0[1], sel1[0], sel1[1]);
 				select = 0;
 				nmark = 0;
 			}
+
 			if(mousebegin(inp) == Mouse2){
 				printf("mouse2!\n");
 			}
@@ -236,11 +327,20 @@ main(int argc, char *argv[])
 			}
 
 			if(mousebegin(inp) == Mouse4){
-				voff -= 30;
+				voff -= linespace();
+				if(select){
+					sel1[1] += linespace();
+					nmark = 0;
+				}
 			}
 			if(mousebegin(inp) == Mouse5){
-				voff += 30;
+				voff += linespace();
+				if(select){
+					sel1[1] -= linespace();
+					nmark = 0;
+				}
 			}
+
 		}
 		if(1){
 			/* clear to transparent */
@@ -259,8 +359,8 @@ main(int argc, char *argv[])
 			sel1fix[1] = sel1[1] < textr.v0 ? textr.v0+voff : (sel1[1] + voff);
 			drawtext(textr, sel0fix, sel1fix);
 
-			/* blend background in */
-			//drawblend_back(&screen, screen.r, bgcolor);
+			/* put background in */
+			blend_add_under(&screen, screen.r, bgcolor);
 		}
 	}
 
